8146e1579d5dc16207695cf492251d71c13b5808
Flitch News <contact@flitch.news>
2020-05-10 20:36:43 +0200

    Create build.js

---
+++ js/build.js
@@ -0,0 +1,20 @@
+
+const adUnits = [
+    {
+        code: 'block_18519', // Article Top 728x90
+        placement_id: 18519,
+        sizes: [728, 90]
+    },
+    {
+        code: 'block_18520', // Article Bottom 728x90
+        placement_id: 18520,
+        sizes: [728, 90]
+    }
+];
+
+let build = () => {
+    smartyads.buildUnits(adUnits);
+};
+
+window.onload = build;
+
