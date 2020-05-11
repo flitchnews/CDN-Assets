60f98c9e8be38aa668384473b60540f5c6bf05aa
Flitch News <contact@flitch.news>
2020-05-11 17:47:32 +0200

    update

---
+++ js/adunits.js
@@ -0,0 +1,20 @@
+
+var adUnits = [
+    {
+        code: 'block_18519', // Article Top 728x90
+        placement_id: 18519,
+        sizes: [728, 90]
+    },
+    {
+        code: 'block_18520', // Article Bottom 728x90
+        placement_id: 18520,
+        sizes: [728, 90]
+    },
+    {
+        code: 'block_18541', // Side Right Top
+        placement_id: 18541,
+        sizes: [300, 250]
+    }
+];
+
+smartyads.buildUnits(adUnits);
